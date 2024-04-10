d8f9c60a2ace398a6397fd1c78adc27aeff1aed0
jams924 <166586029+jams924@users.noreply.github.com>
2024-04-10 07:13:05 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+       <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>Document</title>
+</head>
+<body>
+       <p>bonjour la famillia ceci est la tige</p>
+       <h1>La force de la formation et du savoir faire</h1>
+       <h2>Savoir les chose</h2>
+       <h3>Bien gerer le cahier</h3>
+       <?php
+       #$definition = fopen("manupuler.php", "r+");
+       #$affichagef = fread($definition, 1000);
+       #echo $affichagef;
+       #fclose($definition);
+
+       ?>
+<p>
+       <?php 
+       
+       ?> 
+       </p>           
+</html>
+</body>
